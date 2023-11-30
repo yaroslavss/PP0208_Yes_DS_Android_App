2823dfa35740e3ac1fb1417c8f4d8b06c4b71ebb
yaroslavss <yaroslav.syuzev@gmail.com>
2023-11-30 20:42:56 +0400

    Add teacher's details fragment draft

--- app/src/main/java/ru/yesds/yesdsapp/model/Teacher.kt
+++ app/src/main/java/ru/yesds/yesdsapp/model/Teacher.kt
@@ -1,8 +1,16 @@
 package ru.yesds.yesdsapp.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Teacher(
     val name: String,
     val isLeader: Boolean,
     val profession: String,
-    val photo_min: Int
-)
+    val photo_min: Int,
+    val photo_big: Int,
+    val description: String,
+    val dances_since: String,
+    val teaches_since: String
+) : Parcelable
--- app/src/main/java/ru/yesds/yesdsapp/ui/viewmodel/TeachersViewModel.kt
+++ app/src/main/java/ru/yesds/yesdsapp/ui/viewmodel/TeachersViewModel.kt
@@ -7,42 +7,72 @@ import ru.yesds.yesdsapp.R
 import ru.yesds.yesdsapp.model.Teacher
 
 class TeachersViewModel : ViewModel() {
+    private val description = "О танцевальном пути Екатерины:\n" +
+            "За свою танцевальную карьеру танцует такие стили как Hip-hop, Jazz-Funk, Dancehall и TWERK. \n" +
+            "Участвовала в фестивалях Project 818,Fame your Choreo, также на различных городских фестивалях. На данный момент танцует и преподает TWERK и dancehall. Старается развиваться и в других стилях, \n" +
+            "но предпочтение отдает более женственным направлениям.❤\n" +
+            "\n" +
+            "Проходила мастер-классы у потрясающих танцоров, таких как: Андрей Бойко, Паша Трутнев, DHK Mad Mike, Riley Legacy, Карина Галлямова, DHQ Daha Ice-Cream, Shorty Explora, Саныч Степченко, JEO,Nargiz Radz,Крис и Алия Bangrang, Лилу Кравцова, Claudio Blackeagle, Keatlin, Rafa Redvolcon, Claudio Blackeagle.";
     private val teacherList = listOf(
         Teacher(
             name = "Иван Маланьин",
             isLeader = true,
             profession = "Ведущий преподаватель по ХАСТЛУ",
-            photo_min = R.drawable.img_min_teacher
+            photo_min = R.drawable.img_min_teacher,
+            photo_big = R.drawable.img_big_teacher,
+            description = description,
+            dances_since = "Танцует с 2012 года",
+            teaches_since = "Преподает с 2016 года"
         ),
         Teacher(
             name = "Наталья Алекперова",
             isLeader = true,
             profession = "Ведущий преподаватель по ХАСТЛУ",
-            photo_min = R.drawable.img_min_teacher
+            photo_min = R.drawable.img_min_teacher,
+            photo_big = R.drawable.img_big_teacher,
+            description = description,
+            dances_since = "Танцует с 2012 года",
+            teaches_since = "Преподает с 2016 года"
         ),
         Teacher(
             name = "Игорь Буйлин",
             isLeader = false,
             profession = "Преподаватель по ХАСТЛУ",
-            photo_min = R.drawable.img_min_teacher
+            photo_min = R.drawable.img_min_teacher,
+            photo_big = R.drawable.img_big_teacher,
+            description = description,
+            dances_since = "Танцует с 2012 года",
+            teaches_since = "Преподает с 2016 года"
         ),
         Teacher(
             name = "Екатерина Брякунова",
             isLeader = false,
             profession = "Преподаватель по ХАСТЛУ",
-            photo_min = R.drawable.img_min_teacher
+            photo_min = R.drawable.img_min_teacher,
+            photo_big = R.drawable.img_big_teacher,
+            description = description,
+            dances_since = "Танцует с 2012 года",
+            teaches_since = "Преподает с 2016 года"
         ),
         Teacher(
             name = "Юлия Ланкина",
             isLeader = false,
             profession = "Преподаватель по ДЖАЗ-ФАНКУ и STRIP",
-            photo_min = R.drawable.img_min_teacher
+            photo_min = R.drawable.img_min_teacher,
+            photo_big = R.drawable.img_big_teacher,
+            description = description,
+            dances_since = "Танцует с 2012 года",
+            teaches_since = "Преподает с 2016 года"
         ),
         Teacher(
             name = "Юлия Ланкина",
             isLeader = false,
             profession = "Преподаватель по ДЖАЗ-ФАНКУ и STRIP",
-            photo_min = R.drawable.img_min_teacher
+            photo_min = R.drawable.img_min_teacher,
+            photo_big = R.drawable.img_big_teacher,
+            description = description,
+            dances_since = "Танцует с 2012 года",
+            teaches_since = "Преподает с 2016 года"
         ),
     )
     private val _teacherListLiveData = MutableLiveData<List<Teacher>>()
